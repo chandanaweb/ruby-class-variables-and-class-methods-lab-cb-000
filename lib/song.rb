06ca7cbbdab38d87dfd90d2c3bd9783bcb3a94a8
chandana M <chandu11288@gmail.com>
2018-04-11 01:11:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,14 @@
 class Song
+  @count  = 0
+  @artist
   attr_accessor :name , :genre, :artist
   def initialize(name,artist,genre)
     @name = name
     @genre = genre
     @artist = artist
+    @count += 1
+  end
+  def count
+    @count
   end
 end
