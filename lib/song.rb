8ffd8ed9212c03cd13a0b9888e4d0460fa968c27
chandana M <chandu11288@gmail.com>
2018-04-11 01:24:29 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,4 +15,17 @@ class Song
   def self.count
     @@count
   end
+  def self.genres
+    genres = []
+      @@genres.each do|genre|
+          if genres.include? genre
+            genres
+          else
+            genres << genre
+          end
+      end
+      genres
+  end
+  def self.artists
+  end
 end
